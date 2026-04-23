5198dc6187da7c288078b3a4e1fbbaf01366c987
wissal <wissalbenallam@gmail.com>
2026-04-23 19:26:20 +0100

    Initialisation propre sans dossiers OneDrive

---
+++ CalculTransportService.cs
@@ -0,0 +1,22 @@
+﻿namespace TMS_Project.Services
+{
+    public class CalculTransportService
+    {
+        // Calcul pour le DEVIS (Client)
+        public decimal CalculerDevis(int poids, decimal distanceEstimee)
+        {
+            decimal prixBase = 100; // Frais fixes
+            decimal prixAuKg = 0.5m;
+            decimal prixAuKm = 1.2m;
+
+            return prixBase + (poids * prixAuKg) + (distanceEstimee * prixAuKm);
+        }
+
+        // Calcul pour la TOURNEE (Réel)
+        public decimal CalculerCoutReel(decimal distanceReelle, decimal consommationCamion, decimal prixCarburant)
+        {
+            // Exemple : (Distance / 100 * Consommation) * Prix du litre
+            return (distanceReelle / 100 * consommationCamion) * prixCarburant;
+        }
+    }
+}
